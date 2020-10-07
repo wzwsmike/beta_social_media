28f5cd171240fd407eca0c8cdd4a36f63a7ad56b
chalvinwz <58414592+chalvinwz@users.noreply.github.com>
2020-10-07 14:59:23 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -1,93 +1,57 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Signin</title>
+	<title>Log in</title>
+	<link rel="stylesheet" type="text/css" href="login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
-<style>
-    
-    body{
-        overflow-x: hidden;
-    }
-    
-    .main_content{
-        width: 50%;
-        height: 40%;
-        margin: 10px auto;
-        background-color: #fff;
-        border: 2px solid #e6e6e6;
-        padding: 40px 50px;
-    }
-    
-    .header{
-        border: 0px solid #000;
-        margin-bottom: 5px;
-    }
-    
-    .well{
-        background-color: #D6893A;
-    }
-    
-    #login{
-        width: 40%;
-        border-radius: 50px;
-        background-color:#D6893A;
-        border: 1px solid #D6893A;
-    }
-    
-    #login:hover{
-        width: 40%;
-        border-radius:30px;
-        background-color:#AB6D2E;
-        border:1px solid #D6893A;
-    }
-    
-     .overlap-text{
-        position: relative;
-    }
-    
-    .overlap-text a{
-        position: absolute;
-        top: 8px;
-        right: 10px;
-        font-size: 14px;   
-        text-decoration: none;
-        font-family: 'Overpass Mono', monospace;
-        letter-spacing: -1px;
-    }
-</style>
 <body>
-<div class="row">
-        <div class="col-sm-12">
-            <div class="well">
-                <center><h1 style="color:white;">Shouter</h1></center>
-            </div>
-        </div>
-</div>
-<div class="row">
-    <div class="col-sm-12">
-        <div class="main_content">
-            <div class="header">
-                <h3 style="text-align : center;"><strong>Login to Shouter</strong></h3>
-            </div>
-            <div class="1-part">
-                <form action="" method="post">
+	<div class="container">
+		<div class="card">
+			<div class="hero">
+				<img src="img/hero.png">
+			</div>
+
+			<div class="content">
+				<div class="header">
+					<img src="img/logo.jpg">
+					<button id="signup" class="signupbtn" name="login">Sign up</button><br><br>
+                <?php
+                    if(isset($_POST['signup'])){
+                        echo "<script>window.open('login.php','_self')</script>";
+                    }
+                ?>
+				</div>
+				<div class="title">
+					<h1>Log in and Shout <hr></h1>
+				</div>
+				<div class="form-input">
+					<form action="" method="post">
                     <input type="number" name="number" placeholder="Number" required="required" class="form-control input-md"><br>
                     <div class="overlap-text">
                         <input type="password" name="pass" placeholder="Password" required="required" class="form-control input-md">
-                        <a style="text-decoration: none; float: right; color: #D6893A;" data-toogle="tooltip" tittle="Reset Password" href="forgotpassword.php">Forgot your pass ?</a><br>
+                        <a style="text-decoration: none; float: right; color:#D6893A;" data-toogle="tooltip" tittle="Reset Password" href="forgotpassword.php">Forgot your pass?</a><br>
                     </div>
-                    <a style="text-decoration: none; float: right; color:#D6893A;" data-toogle="tooltip" title="Create an account" href="signup.php">Don't Have an Account?</a><br><br>
-                    <center><button id="login" class="btn btn-info btn-lg" name="login">Login</button></center>
-                   <?php include("login_code.php"); ?>
+                    <div class="loginbtn">
+                    <button id="login" class="btn btn-info btn-lg" name="login">Login</button>
+                    <a style="text-decoration: none; float: left; color:#D6893A;" data-toogle="tooltip" title="Create an account" href="signup.php">Don't Have an Account?</a>
+                    </div>
+                   <!-- <?php include("login_code.php"); ?> -->
                 </form>
-            </div>
-        </div>
-    </div>
-</div>    
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="footer">
+		<hr>
+		<p>© 2020 Shouter, Inc.</p>
+	</div>
 </body>
-</html>    
+</html>
